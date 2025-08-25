5802ab02b1483c2bf974b14508112398a4dda2d0
darikthegreat <asmamawdarik7@gmail.com>
2025-08-25 03:48:23 +0300

    Update signup.js

--- api/signup.js
+++ api/signup.js
@@ -1,7 +1,7 @@
 
 import { createClient } from '@supabase/supabase-js';
 
-// Initialize Supabase on the backend
+
 const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_KEY);
 
 export default async function handler(req, res) {
